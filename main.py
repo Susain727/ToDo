fe6db9587f3a150b3050bd2a1ad10b6a59bb3724
Susain727 <susaindehran@gmail.com>
2025-08-15 21:48:54 +0530

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+class ToDo_list:
+    def __init__(self):
+        self.task = []
+    
+    def display_menu(self):
+        print("1. Add task")
+        print("2. Edit task")
+        print("3. Delete task")
+        print("4. Exit")
+
+todo = ToDo_list()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+print("1. Add task")
+        print("2. Edit task")
+        print("3. Delete task")
+        print("4. Exit")
